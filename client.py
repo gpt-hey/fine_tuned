d8e28ac8997e5e629b8d5a3407bf2a6542c7d85a
guanw <guanw0826@gmail.com>
2023-12-03 15:04:16 -0500

    remove http server

--- client.py
+++ client.py
@@ -2,16 +2,17 @@ import asyncio
 import websockets
 
 async def send_message():
-    uri = "ws://localhost:8081"  # Replace with the actual WebSocket server URI
-    # uri = "ws://129.213.151.7:8081"
+    uri = "ws://0.0.0.0:8081"  # Replace with the actual WebSocket server URI
 
     async with websockets.connect(uri, ping_timeout=None) as websocket:
-        # message = "@remindme to take notes!"
+        # Send your actual message
         message = "@gguf tell me about yourself"
         print(f"Sending message to server: {message}")
         await websocket.send(message)
+
         while True:
             try:
+                # Receive and print the response from the server
                 response = await websocket.recv()
                 print(f"Received response from server: {response}")
                 if not response:
--- server/websocket_server.py
+++ server/websocket_server.py
@@ -3,7 +3,6 @@ import websockets
 from callbacks import websocket_callback_singleton
 from models.llama2_gguf_model import Llama2GGUFModel
 from remindme_parser import Remindme
-from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 PORT = 8081
 ADDRESS = "0.0.0.0"
@@ -30,26 +29,11 @@ async def message(websocket, path):
     except websockets.exceptions.ConnectionClosed:
         print("Connection closed by the client.")
 
-class HealthCheckHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/healthcheck':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(b'OK')
-        else:
-            super().do_GET()
-
 async def main():
-    websocket_server = websockets.serve(message, ADDRESS, PORT)
-    print(f"WebSocket server started on ws://{ADDRESS}:{PORT}")
+    async with websockets.serve(message, ADDRESS, PORT):
+        print(f"WebSocket server started on ws://{ADDRESS}:{PORT}")
+        await asyncio.Event().wait()
 
-    # Start the HTTP server on the same port
-    http_server = HTTPServer((ADDRESS, PORT), HealthCheckHandler)
-    print(f"HTTP server started on http://{ADDRESS}:{PORT}")
-
-    # Run both servers concurrently
-    await asyncio.gather(websocket_server, http_server.serve_forever())
 
 # Run the WebSocket server
 asyncio.run(main())
