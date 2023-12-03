02a2af570d41c7caa147f8993e0ac39cd5c114bd
guanw <guanw0826@gmail.com>
2023-12-03 14:26:15 -0500

    add simple http healthcheck endpoint

--- server/websocket_server.py
+++ server/websocket_server.py
@@ -3,8 +3,10 @@ import websockets
 from callbacks import websocket_callback_singleton
 from models.llama2_gguf_model import Llama2GGUFModel
 from remindme_parser import Remindme
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 PORT = 8081
+ADDRESS = "0.0.0.0"
 
 async def message(websocket, path):
     print(f"Client connected to path: {path}")
@@ -27,10 +29,27 @@ async def message(websocket, path):
 
     except websockets.exceptions.ConnectionClosed:
         print("Connection closed by the client.")
+
+class HealthCheckHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/healthcheck':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            super().do_GET()
+
 async def main():
-    async with websockets.serve(message, "localhost", PORT):
-        print(f"WebSocket server started on ws://localhost:{PORT}")
-        await asyncio.Event().wait()
+    websocket_server = websockets.serve(message, ADDRESS, PORT)
+    print(f"WebSocket server started on ws://{ADDRESS}:{PORT}")
+
+    # Start the HTTP server on the same port
+    http_server = HTTPServer((ADDRESS, PORT), HealthCheckHandler)
+    print(f"HTTP server started on http://{ADDRESS}:{PORT}")
+
+    # Run both servers concurrently
+    await asyncio.gather(websocket_server, http_server.serve_forever())
 
 # Run the WebSocket server
 asyncio.run(main())
