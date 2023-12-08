c8997b1808e3f75364e05e3dbf2f9cac1ba6a7ca
guanw <guanw0826@gmail.com>
2023-12-07 19:44:49 -0500

    refactor to enable concurrent request (remove singleton callback to avoid leaked semaphore) NOTE: it still needs further perf improvement because we are now init Llama2GGUFModel everytime a new request comes in

--- server/callbacks.py
+++ server/callbacks.py
@@ -1,16 +1,7 @@
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 class WebsocketCallbackHandler(StreamingStdOutCallbackHandler):
-    _instance = None
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(WebsocketCallbackHandler, cls).__new__(cls)
-            # Initialize the singleton instance here
-        return cls._instance
-
-    def set_websocket(self, websocket):
+    def __init__(self, websocket):
         self.websocket = websocket
     async def on_llm_new_token(self, token, **kwargs):
         """Run on new LLM token. Only available when streaming is enabled."""
         await self.websocket.send(token)
-
-websocket_callback_singleton = WebsocketCallbackHandler()
--- server/websocket_server.py
+++ server/websocket_server.py
@@ -1,6 +1,6 @@
 import asyncio
 import websockets
-from callbacks import websocket_callback_singleton
+from callbacks import WebsocketCallbackHandler
 from models.llama2_gguf_model import Llama2GGUFModel
 from remindme_parser import Remindme
 
@@ -9,9 +9,6 @@ ADDRESS = "0.0.0.0"
 
 async def handle_websocket_close(websocket):
     try:
-        # Your WebSocket handling logic goes here
-
-        # To close the WebSocket connection, you can use the close method
         await websocket.close()
 
     except websockets.exceptions.ConnectionClosedOK:
@@ -23,19 +20,19 @@ async def handle_websocket_close(websocket):
 
 async def message(websocket, path):
     print(f"Client connected to path: {path}")
-    websocket_callback_singleton.set_websocket(websocket)
+    callback = WebsocketCallbackHandler(websocket)
     try:
         async for message in websocket:
             print(f"Received message from client: {message}")
             r = Remindme()
             if r.is_matched(message):
-                resp = r.execute_action(message)
+                resp = await asyncio.to_thread(r.execute_action, message)
                 await websocket.send(f'remindme executed: {resp}')
                 return
 
-            gguf_model = Llama2GGUFModel()
+            gguf_model = Llama2GGUFModel(callback)
             if gguf_model.is_matched(message):
-                gguf_model.execute_action(message)
+                await asyncio.to_thread(gguf_model.execute_action, message)
                 return
 
             await websocket.send('voided conversation with no match')
@@ -47,9 +44,9 @@ async def message(websocket, path):
 
 async def main():
     async with websockets.serve(message, ADDRESS, PORT):
+        await asyncio.Future()
         print(f"WebSocket server started on ws://{ADDRESS}:{PORT}")
-        await asyncio.Event().wait()
-
 
 # Run the WebSocket server
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
